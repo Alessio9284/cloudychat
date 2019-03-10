81a9dfc996b6f3a0c559bf8427436577c8fc63c7
Alessio Torricelli <Alessio92845@gmail.com>
2019-03-10 17:41:31 +0100

    pagina login funzionante

--- chat/views.py
+++ chat/views.py
@@ -5,16 +5,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'chat/index.html')
    
 def registration(request):
-    return render(request, 'registration.html')
+    return render(request, 'chat/registration.html')
 
 def userlist(request):
-    return render(request, 'userlist.html')
+    return render(request, 'chat/userlist.html')
 
 def chat(request, nickname):
     #controllare che l'utente inserito esista nella tabella users
     #creare una tabella con gli id dei due utenti
     #pensare al meccanismo di salvataggio
-    return render(request, 'private_chat.html', { 'nickname' : nickname })
+    return render(request, 'chat/private_chat.html', { 'nickname' : nickname })
