079006cf2a05e210e86c871b64ad207b8de70456
Alessio Torricelli <Alessio92845@gmail.com>
2019-03-13 21:00:16 +0100

    SESSIONI FUNZIONANTI

--- chat/views.py
+++ chat/views.py
@@ -10,6 +10,11 @@ from hashlib import md5
 import random
 
 def index(request):
+
+	# Distruzione della sessione
+	for key in list(request.session.keys()):
+		del request.session[key]
+
 	return render(request, 'chat/index.html')
    
 def registration(request):
@@ -29,47 +34,60 @@ def adduser(request):
 
 	if request.method == 'POST':
 
+		# Prendo i dati del Form
 		form = FormDati(request.POST)
 
+		# Controllo che il form sia valido
 		if form.is_valid():
 
+			# Raccolgo i dati in modo da essere utilizzabili
 			dati = form.cleaned_data
 			nickname = dati['nickname']
-			password =  dati['password']
 
+			# Controllo che il nome utente non esista già
 			if not (User.objects.filter(nickname = nickname).exists()):
+
+				password =  dati['password']
 				
+				# Creazione dell'utente tramite un oggetto
 				user = User(
 					nickname = nickname,
 					password = md5(password.encode()).hexdigest(),
 					color = '%06x' % random.randint(0, 0xFFFFFF)
 				)
 
+				# INSERT nel database
 				user.save()
 				
 				return HttpResponseRedirect('../')
-			else:
-				return HttpResponseRedirect('/reg/')
-	else:
-		return HttpResponseRedirect('/reg/')
+			
+			return HttpResponseRedirect('/reg/')
+	
+	return HttpResponseRedirect('/reg/')
 
 def logging(request):
 
 	if request.method == 'POST':
 
+		# Prendo i dati del Form
 		form = FormDati(request.POST)
 
+		# Controllo che il form sia valido
 		if form.is_valid():
 
+			# Raccolgo i dati in modo da essere utilizzabili
 			dati = form.cleaned_data
 
 			nickname = dati['nickname']
 			password = md5(dati['password'].encode()).hexdigest()
-			color = User.objects.filter(nickname = nickname, password = password).values('color')
 
+			# Controllo se l'utente esiste
 			user = User.objects.filter(nickname = nickname, password = password)
 
 			if (user.exists()):
+
+				# Estrazione del colore dall'utente salvato
+				color = User.objects.get(nickname = nickname, password = password).color
 
 				request.session['nickname'] = nickname
 				request.session['password'] = password
